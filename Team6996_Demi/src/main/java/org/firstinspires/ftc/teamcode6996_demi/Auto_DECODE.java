3c7d716c8841d4f7a087da20c12b81bbd903b780
David Flores <156151792+dm-flores@users.noreply.github.com>
2025-10-17 22:36:19 -0400

    Update Auto_DECODE.java
    Add webcam checking of the obelisk
    disable the launcher

--- Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/Auto_DECODE.java
+++ Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/Auto_DECODE.java
@@ -6,30 +6,39 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode6996_demi.mechanisms.Launcher;
 import org.firstinspires.ftc.teamcode6996_demi.mechanisms.MecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Autonomous(name = "Auto_DECODE")
 public class Auto_DECODE extends OpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    private ElapsedTime shotTimer = new ElapsedTime();
-    private ElapsedTime feederTimer = new ElapsedTime();
 
-    final double LAUNCHER_TARGET_VELOCITY = 1125;
-    final double LAUNCHER_MIN_VELOCITY = 1075;
-    final double TIME_BETWEEN_SHOTS = 2;
-    final double FEED_TIME = 0.20;
+
+
+
     int shotsToFire = 3; //The number of shots to fire in this auto.
     double robotRotationAngle = 45;
 
     private MecanumDrive robot;
+    private Launcher launcher;
 
     private enum Alliance {
         NOT_SET,
         BLUE,
-        RED;
+        RED
     }
     private Alliance alliance = Alliance.NOT_SET;
 
@@ -37,12 +46,13 @@ public class Auto_DECODE extends OpMode {
         NOT_SET,
         START_LOCATION_1,
         START_LOCATION_2,
-        START_LOCATION_3;
+        START_LOCATION_3
     }
     private Location start_location = Location.NOT_SET;
 
     public enum AprilTags {
-        ALL(-1), BLUE_GOAL(20), RED_GOAL(24),
+        ANY(-1), NONE(0),
+        BLUE_GOAL(20), RED_GOAL(24),
         OBELISK_GPP(21), OBELISK_PGP(22), OBELISK_PPG(23);
 
         private AprilTags(final int id)
@@ -56,28 +66,26 @@ public class Auto_DECODE extends OpMode {
         private int id;
     }
 
-    private enum LaunchState {
-        IDLE,
-        PREPARE,
-        LAUNCH,
-    }
-    private LaunchState launchState;
-
     private enum AutonomousState {
+        START,
         LAUNCH,
         WAIT_FOR_LAUNCH,
         DRIVING_AWAY_FROM_GOAL,
         ROTATING,
         DRIVING_OFF_LINE,
-        COMPLETE;
+        COMPLETE
     }
-    private AutonomousState autonomousState;
+    private AutonomousState autonomousState = AutonomousState.START;
 
     public Gamepad saved_gamepad1 = new Gamepad();
-    public String audience = "none";
-    public String backside = "none";
-    public String position = "none";
 
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private static final AprilTags DESIRED_TAG_ID = AprilTags.ANY;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private VisionPortal visionPortal;               // Used to manage the video source.
+    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    private boolean targetFound = false;    // Set to true when an AprilTag target is detected
+    private AprilTags targetFoundTag = AprilTags.NONE;
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -86,6 +94,12 @@ public class Auto_DECODE extends OpMode {
         // Hardware map setup
         robot = new MecanumDrive();
         robot.init(hardwareMap);
+
+        initAprilTag();
+
+        //launcher =  new Launcher();
+        //launcher.init(hardwareMap);
+
         // Constants for wheel diameter am-4763 ?
         robot.setWheelDiameter(100);
         // Constants for encoder counts for AM-2964 AndyMark NeveRest 40 (40:1)
@@ -127,6 +141,7 @@ public class Auto_DECODE extends OpMode {
     @Override
     public void start() {
         runtime.reset();
+        autonomousState = AutonomousState.START;
     }
 
     /*
@@ -137,31 +152,39 @@ public class Auto_DECODE extends OpMode {
         // Setup a variable for each drive wheel to save power level for telemetry
         double DRIVE_SPEED = .5;
 
+        if (!targetFound)
+        {
+            lookingForTag();
+        }
         switch (autonomousState){
             case LAUNCH:
-                launch(true);
+                //launcher.launch(true);
                 autonomousState = AutonomousState.WAIT_FOR_LAUNCH;
                 break;
 
             case WAIT_FOR_LAUNCH:
-                if(launch(false)) {
+                /*
+                if(launcher.launch(false)) {
                     shotsToFire -= 1;
                     if(shotsToFire > 0) {
                         autonomousState = AutonomousState.LAUNCH;
                     } else {
                         robot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                       // launcher.setVelocity(0);
+                        launcher.setVelocity(0);
                         autonomousState = AutonomousState.DRIVING_AWAY_FROM_GOAL;
                     }
                 }
+                */
                 break;
 
             case DRIVING_AWAY_FROM_GOAL:
-
+/*
                 if(robot.drive(DRIVE_SPEED, -4, DistanceUnit.INCH, 1)){
                     robot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                     autonomousState = AutonomousState.ROTATING;
                 }
+
+ */
                 break;
 
             case ROTATING:
@@ -181,14 +204,20 @@ public class Auto_DECODE extends OpMode {
                 break;
 
             case DRIVING_OFF_LINE:
+                /*
                 if(robot.drive(DRIVE_SPEED, -26, DistanceUnit.INCH, 1)){
                     autonomousState = AutonomousState.COMPLETE;
                 }
+
+                 */
+                break;
+            default:
                 break;
         }
 
         telemetry.addData("AutoState", autonomousState);
-        telemetry.addData("LauncherState", launchState);
+        telemetry.addData("Obelisk", targetFoundTag);// targetFound ? desiredTag.id : "NONE");
+       //telemetry.addData("LauncherState", launcher.getLaunchState());
         outputPositions("Current", robot.getAllPositions());
         outputPositions("Target", robot.getAllTargetPositions());
         telemetry.update();
@@ -282,43 +311,126 @@ public class Auto_DECODE extends OpMode {
     }
 */
 
-    boolean launch(boolean shotRequested){
-        switch (launchState) {
-            case IDLE:
-                if (shotRequested) {
-                    launchState = LaunchState.PREPARE;
-                    shotTimer.reset();
-                }
-                break;
-            case PREPARE:
-                /*
-                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
-                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY){
-                    launchState = LaunchState.LAUNCH;
-                    //leftFeeder.setPower(1);
-                    //rightFeeder.setPower(1);
-                    feederTimer.reset();
-                }
-
-                 */
-                break;
-            case LAUNCH:
-                if (feederTimer.seconds() > FEED_TIME) {
-                   // leftFeeder.setPower(0);
-                    //rightFeeder.setPower(0);
-
-                    if(shotTimer.seconds() > TIME_BETWEEN_SHOTS){
-                        launchState = LaunchState.IDLE;
-                        return true;
-                    }
-                }
-        }
-        return false;
-    }
-
     public void telemetryChoice() {
+        telemetry.addData("Version", "3");
         telemetry.addData("Current Alliance", alliance);
         telemetry.addData("Start Position", start_location);
         telemetry.update();
+    }
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // e.g. Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        // Create the vision portal by using a builder.
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+
+    private void mySleep(int milliseconds)
+    {
+        ElapsedTime timer = new ElapsedTime();
+        int x = 0;
+        while(timer.milliseconds() < milliseconds)
+        {
+            x++;
+        }
+    }
+    /*
+         Manually set the camera gain and exposure.
+         This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+        */
+    private void setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        runtime.startTime();
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while ((visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                mySleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+
+        ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+        if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+            exposureControl.setMode(ExposureControl.Mode.Manual);
+            mySleep(50);
+        }
+        exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+        mySleep(20);
+        GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+        gainControl.setGain(gain);
+        mySleep(20);
+    }
+
+    private void lookingForTag() {
+        targetFound = false;
+        desiredTag = null;
+
+        // Step through the list of detected tags and look for a matching tag
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detection : currentDetections) {
+            // Look to see if we have size info on this tag.
+            if (detection.metadata != null) {
+                //  Check to see if we want to track towards this tag.
+                if ((detection.id == AprilTags.OBELISK_GPP.getID()) ||
+                    (detection.id == AprilTags.OBELISK_PGP.getID()) ||
+                    (detection.id == AprilTags.OBELISK_PPG.getID()))
+                {
+                    // Yes, we want to use this tag.
+                    targetFound = true;
+                    desiredTag = detection;
+                    if (detection.id == AprilTags.OBELISK_GPP.getID())
+                    {
+                        targetFoundTag = AprilTags.OBELISK_GPP;
+                    }
+                    if (detection.id == AprilTags.OBELISK_PGP.getID())
+                    {
+                        targetFoundTag = AprilTags.OBELISK_PGP;
+                    }
+                    if (detection.id == AprilTags.OBELISK_PPG.getID())
+                    {
+                        targetFoundTag = AprilTags.OBELISK_PPG;
+                    }
+                    break;  // don't look any further.
+                } else {
+                    // This tag is in the library, but we do not want to track it right now.
+                    telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                }
+            } else {
+                // This tag is NOT in the library, so we don't have enough information to track to it.
+                telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+            }
+        }
     }
 }
