b1023e4d295e72111493167532a9d0ace8c93e01
Camy15 <camdenrenner15@gmail.com>
2025-12-04 18:17:23 -0500

    Updates for using GoBilda PinPoint with turn to point.

--- Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/test.java
+++ Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/test.java
@@ -29,8 +29,9 @@ public class test extends OpMode{
         robot.PinPointUpdate();
         //robot.getPinpointPosition();
         //if(robot. != null){
-            double stopTurnRangeDeg = 70;//deg
-            double targetHeadingDeg = 190;// angle that the robot ends at in deg
+            double slowSpeed = 0.010;
+            double stopTurnRangeDeg = 5;//deg
+            double targetHeadingDeg = 567;// angle that the robot ends at in deg
             double startAt = 0;
 
             //if (targetHeadingDeg < 0){
@@ -41,12 +42,23 @@ public class test extends OpMode{
             double currentHeadingDeg = startAt-(robot.getPinpointPosition().getHeading(AngleUnit.DEGREES));
             double moveDeg = angleError(targetHeadingDeg,currentHeadingDeg);
             double thing = targetHeadingDeg -currentHeadingDeg;
+            double moveDegAbs = 0;
             telemetry.addData("PinPointx in MM", PinPointx);
             telemetry.addData("PinPointy in MM", PinPointy);
             telemetry.addData("currentHeading in DEG", currentHeadingDeg);
             telemetry.addData("targetHeadingDeg", targetHeadingDeg);
             telemetry.addData("moveDeg", moveDeg);
             telemetry.addData("thing", thing);
+
+            //Translator{
+            moveDegAbs = Math.abs(moveDeg*slowSpeed);
+            moveDeg = moveDeg*slowSpeed;
+            if(moveDegAbs <= 0.1) {
+                moveDeg = 0;
+            }
+
+            telemetry.addData("moveDeg",moveDeg);
+            //}
 
             robot.move(0,0,moveDeg);
 
