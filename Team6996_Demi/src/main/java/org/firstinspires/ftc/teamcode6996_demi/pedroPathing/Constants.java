fe284f71173e6493db553242c0b763d540a19242
Camy15 <camdenrenner15@gmail.com>
2025-12-14 15:42:44 -0500

    Updates for using GoBilda PinPoint with turn to point.

--- Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/pedroPathing/Constants.java
+++ Team6996_Demi/src/main/java/org/firstinspires/ftc/teamcode6996_demi/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode6996_demi.pedroPathing;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
@@ -9,12 +11,48 @@ import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
+    public void initOthers(){
+        /*
+        launch_motor = hardwareMap.get(DcMotor.class, "launch_motor");
+        intake_motor = hardwareMap.get(DcMotor.class, "intake_motor");
+
+        turret_hood_servo = hardwareMap.get(Servo.class, "turret_hood_servo");
+        turret_left_servo = hardwareMap.get(Servo.class, "turret_left_servo");
+        turret_right_servo = hardwareMap.get(Servo.class, "turret_right_servo");
+        turret_feeder_servo = hardwareMap.get(Servo.class, "turret_feeder_servo");
+        park_lift_servo = hardwareMap.get(Servo.class, "park_lift_servo");
+        rgb_servo = hardwareMap.get(RevBlinkinLedDriver.class, "rgb_servo");
+
+        turret_encoder = hardwareMap.get(DcMotor.class, "turret_encoder");
+        turret_left_limit_sensor = hardwareMap.get(DigitalChannel.class, "turret_left_limit_sensor");
+        turret_right_limit_sensor = hardwareMap.get(DigitalChannel.class, "turret_left_limit_sensor");
+        left_light_sensor = hardwareMap.get(ColorSensor.class, "left_light_sensor");
+        right_light_sensor = hardwareMap.get(ColorSensor.class, "right_light_sensor");
+        back_light_sensor = hardwareMap.get(ColorSensor.class, "back_light_sensor");
+        blinkin = hardwareMap.get(RevBlinkinLedDriver.class, "blinkin");
+
+        turret_encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret_encoder.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turret_left_limit_sensor.setMode(DigitalChannel.Mode.INPUT);
+        turret_right_limit_sensor.setMode(DigitalChannel.Mode.INPUT);
+        left_light_sensor.enableLed(true); // Turn on the white LED if true
+        right_light_sensor.enableLed(true); // Turn on the white LED if true
+        back_light_sensor.enableLed(true); // Turn on the white LED if true
+         */
+    }
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(5)//must be in kilograms
             .forwardZeroPowerAcceleration(-41.69401197278003)
