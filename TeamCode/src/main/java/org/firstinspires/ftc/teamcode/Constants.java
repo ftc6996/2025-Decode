906972a3ef92cfcfca30afbb673670f4646b7dc3
Camy15 <camdenrenner15@gmail.com>
2026-01-21 21:36:41 -0500

    tweak numbers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -28,9 +28,9 @@ public interface Constants {
     }
 
     public interface Launcher {
-        static final double kHOOD_MIN_POS = 1.0; //0.6;
-        static final double kHOOD_MAX_POS = 0.6; //1.0;
-        static final int kLAUNCHER_TARGET_VELOCITY_CLOSE = 800; //1125;
+        static final double kHOOD_MIN_POS = 1.0;
+        static final double kHOOD_MAX_POS = .6;
+        static final int kLAUNCHER_TARGET_VELOCITY_CLOSE = 850; //1125;
         static final int kLAUNCHER_TARGET_VELOCITY_FAR = 1150;
         static final double kFEED_TIME_SECONDS = 1;
         static final double kFEED_OPEN_POS = 0;
